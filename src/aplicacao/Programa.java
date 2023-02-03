a8e640d7e9d746084dc781972c0feb2d27d65b1b
Lucasdaniel314 <lucasdanielmonteirodesousa827@gmail.com>
2023-02-03 15:52:16 -0300

    programa com erros no remover()

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,64 @@
+package aplicacao;
+
+import java.util.Date;
+import java.util.Scanner;
+
+import entidade.Carrinho;
+import entidade.Cliente;
+import entidade.Produto;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		int resp = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("bem vindo ao mercadinho Lucas!");
+		System.out.println("para iniciarmos, você deve digitar seu dados:");
+		System.out.print("nome: ");
+		String nome = sc.nextLine();
+		System.out.print("id (numero de 4 digitos): ");
+		int id = sc.nextInt();
+		System.out.print("email: ");
+		sc.next();
+		String email = sc.nextLine();
+		Cliente cliente = new Cliente(nome, email, id);
+		
+		
+		System.out.println("vamos adcionar um produto!");
+		System.out.print("nome do produto: ");
+		String nomeproduto = sc.nextLine();
+		System.out.print("preço: ");
+		double preco = sc.nextDouble();
+		Produto produto = new Produto(nomeproduto, preco);
+		Carrinho carrinho = new Carrinho(new Date(), cliente);
+		carrinho.adcionarProduto(produto);
+		
+		do {
+			System.out.println("menu: ");
+			System.out.println("[1] adcionar produto");
+			System.out.println("[2] remover produto");
+			System.out.println("[3] mostar resumo");
+			System.out.println("[4] encerrar programa");
+			resp = sc.nextInt();
+			switch (resp) {
+				case 1:
+					carrinho.adcionar();
+					break;
+				case 2:
+					carrinho.remover();
+					break;
+				case 3:
+					System.out.println(carrinho.toString());
+					break;
+				case 4:
+					break;
+				default:
+					System.out.println("código inválido");
+					break;
+			}
+		}while(resp != 4);
+		
+		sc.close();
+	}
+
+}
---
+++ src/entidade/Carrinho.java
@@ -0,0 +1,111 @@
+package entidade;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Carrinho {
+	
+	/**
+	 * olá, este é o meu primeiro javadoc 
+	 * 
+	 * este programa simula uma loja que tem um carrinho
+	 * @author Lucas Daniel
+	 */
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	Scanner sc = new Scanner(System.in);
+	
+	private Date data;
+	private Cliente cliente;
+	private Produto produto;
+	
+	List<Produto> lista = new ArrayList<>();
+	
+	public Carrinho() {
+	}
+
+	public Carrinho(Date data, Cliente cliente) {
+		this.data = data;
+		this.cliente = cliente;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public List<Produto> getLista() {
+		return lista;
+	}
+
+	public void adcionarProduto(Produto produto) {
+		lista.add(produto);
+	}
+	
+	public void removerProduto(Produto produto) {
+		lista.remove(produto);
+	}
+	
+	public void adcionar() {
+		System.out.print("nome do produto: ");
+		String nomeproduto = sc.nextLine();
+		System.out.print("preço: ");
+		double preco = sc.nextDouble();
+		Produto produto = new Produto(nomeproduto, preco);
+		adcionarProduto(produto);
+	}
+	
+	public void remover() {
+		int x;
+		System.out.print("o que quer remover? ");
+		sc.next();
+		String hj = sc.nextLine();
+		//ajuda
+		x = lista.indexOf(hj);
+		lista.remove(x);
+	}
+	
+	public double total() {
+		double soma = 0.0;
+		for(Produto item : lista) {
+			soma += item.getPreco();
+		}
+		return soma;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("resumo do carrinho" + "\n");
+		sb.append("data: ");
+		sb.append(sdf.format(data) + "\n");
+		for (Produto item: lista) {
+			sb.append(item + "\n");
+		}
+		sb.append("total: ");
+		sb.append(total());
+		return sb.toString();
+	}
+	
+}
